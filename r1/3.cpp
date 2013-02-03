567aed3e0725a77e973c6cd25ad42e02579b3b5c
hanson <hangzhong.yu@gmail.com>
2013-02-03 21:10:29 +0800

    3

--- r1/3.cpp
+++ r1/3.cpp
@@ -3,7 +3,8 @@
 #include <vector>
 using namespace std;
 
-long w, h, p, q, n, X, Y, a, b, c, d;
+int w, h, p, q;
+long n, X, Y, a, b, c, d;
 long long ans;
 
 class P
@@ -33,16 +34,17 @@ void test_deadps()
 
 void read()
 {
-    scanf("%ld %ld %ld %ld %ld %ld %ld %ld %ld %ld %ld", &w, &h, &p, &q, &n, &X, &Y, &a, &b, &c, &d);
+    scanf("%d %d %d %d %ld %ld %ld %ld %ld %ld %ld", &w, &h, &p, &q, &n, &X, &Y, &a, &b, &c, &d);
 }
 
 void init()
 {
     //should rethink
-    //
-    ans = (w-p)*(h-q);
-}
+    //x: 0 -- (w-p)
+    //y: (q-1) -- (h-1)
 
+    ans = (w-p+1)*(h-q+1);
+}
 
 //
 bool compare(P* tp, P* other)
@@ -75,7 +77,7 @@ void rmDup()
     ps.clear();
     ps.push_back(deadps[0]);
 
-    test_deadps();
+    //test_deadps();
     for (int i = 1; i < deadps.size(); ++i)
     {
         if (deadps[i]->x!=ps[ps.size()-1]->x && deadps[i]->y!=ps[ps.size()-1]->y)
@@ -83,7 +85,7 @@ void rmDup()
             ps.push_back(deadps[i]);
         }
     }
-    test_ps();
+    //test_ps();
 }
 
 void make_dead_p()
@@ -115,10 +117,54 @@ void output(int tid)
     cout<<"Case #"<<tid<<": "<<ans<<endl;
 }
 
+void work()
+{
+    test_ps();
+    int flag = 1;
+    for (int num = 1; num <= ps.size(); ++num)
+    {
+        flag *= -1;
+        for (int k = 0; k < ps.size(); k += num)
+        {
+            int leftx = ps[k]->x;
+            int rightx = ps[k]->x;
+            int upy = ps[k]->y;
+            int downy = ps[k]->y;
+
+            for (int tk = k + 1; tk < (k+num) && tk<ps.size(); ++tk)
+            {
+                if (ps[tk]->x < leftx)
+                {
+                    leftx = ps[tk]->x;
+                }
+                if (ps[tk]->x > rightx)
+                {
+                    rightx = ps[tk]->x;
+                }
+                if (ps[tk]->y < upy)
+                {
+                    upy = ps[tk]->y;
+                }
+                if (ps[tk]->y > downy)
+                {
+                    downy = ps[tk]->y;
+                }
+            }
+            //leftx, rightx;
+            //upy, downy;
+            //x:max(0, rightx-p+1) -- min(w-1, leftx + p-1);
+            //y:max(0, downy-q+1) -- min(h-1, upy+q-1)
+        
+            ans += flag*( (1+min(w-1, leftx+p-1)-max(0, rightx-p+1)) * (1+min(h-1, upy+q-1)-max(0, downy-q+1)) );
+            cout<<"ans = "<<ans<<endl;
+        }
+    }
+}
+
 int main()
 {
-    freopen("3.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
+    //freopen("3.txt", "r", stdin);
+    //freopen("out.txt", "w", stdout);
     int test;
     scanf("%d", &test);
     for (int tid = 1; tid<=test; ++tid)
@@ -127,8 +173,8 @@ int main()
         //sory by x, y and no dup
         make_dead_p();
         init();
-        //
-        //work();
+        
+        work();
         output(tid);
     }
 
